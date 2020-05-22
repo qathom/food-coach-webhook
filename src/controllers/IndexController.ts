90805e4ae8fc24d7c842bfa1ff624ff2b8a4923b
qathom <anthony.me@protonmail.ch>
2020-05-22 22:30:03 +0200

    Added i18n text

--- src/controllers/IndexController.ts
+++ src/controllers/IndexController.ts
@@ -1,9 +1,28 @@
-import { Get, Controller, Render } from 'routing-controllers';
+import { Get, Controller, Render, QueryParam } from 'routing-controllers';
 import * as path from 'path';
 
 @Controller()
 export class IndexController {
   @Get('')
   @Render(path.join('index.html'))
-  index() {}
+  index(@QueryParam('lang') inputLanguage: string) {
+    const i18n = {
+      en: {
+        name: 'FoodCoach-bot is at your service',
+        description: 'Use the chat in the bottom right corner',
+        language: 'Language',
+        languageCode: 'en',
+      },
+      fr: {
+        name: 'FoodCoach-bot est à votre service',
+        description: 'Utilisez le chat en bas à droite',
+        language: 'Langue',
+        languageCode: 'fr',
+      },
+    };
+
+    const language = Object.keys(i18n).includes(inputLanguage) ? inputLanguage : 'en';
+
+    return i18n[language];
+  }
 }
